e11d8df6463afb969373f1f4552effe589be24ef
0xpranjl <Pranjal.maurya402@gmail.com>
2023-02-05 15:26:17 +0530

    first n final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 function App() {
+  const [sugg,ssugg]=useState(false);
+  const [code,scode]=useState();
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCat3fPZxlaDjwBusN31FCweniLvRXTHP4",
+    authDomain: "anonsuggestion-e2f64.firebaseapp.com",
+    projectId: "anonsuggestion-e2f64",
+    storageBucket: "anonsuggestion-e2f64.appspot.com",
+    messagingSenderId: "453276583422",
+    appId: "1:453276583422:web:60226e065214517799ecaf",
+    measurementId: "G-0G8HTFEFYD"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <br></br>
+    <br></br>
+    {!sugg?<div>
+    <h2>Enter the Last 3 digits of your mobile number to verify its you</h2>
+    <input style={{marginTop:50, fontSize:30}} placeholder="###" onChange={(e)=>{
+    scode(e.target.value);
+    }} type="number" height="50px"></input>
+    <br></br>
+    <br></br>
+    <Button variant="outline-success" onClick={()=>{
+    if(code==269){
+      ssugg(true);
+    }
+    else{
+      alert("will open without correct answer try 999 times for breaking the code!! ")
+    }
+    }}>show suggestion</Button>
+    </div>:<div style={{padding:50,fontFamily:"cursive"}}>
+     <h3>
+     1) Jo pehla wala gaana tha woh bhul jaao. "kahani suno" song suni ho woh gaa do bahut mast lagta aaj sunday hai agar free ho toh yaa jab bhi free hona tab
+      <br></br>
+      <br></br>
+      2)aur jo bhi gaana gaya karo woh upload kardiya karo and jo usme se mast wala part rehta hai na usko trim karke woh youtube shorts bana ke upload kardiya karo
+      youtube shorts mein SEO shi rehta kaafi view milta apne aap.
+     </h3>
+  <br></br>
+  <h1>REPLY IN THE COMMENT SECTION😂😂</h1>
+      
+      </div>}
     </div>
   );
 }
